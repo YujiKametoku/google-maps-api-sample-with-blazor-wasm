6822012cc0e31f0144beaf50eddad4a306ce2be0
yuji kametoku <yuji.kametoku@gmail.com>
2022-01-31 22:00:27 +0900

    プロジェクト ファイルを追加します。

---
+++ wasm_google_map_sample/Client/wwwroot/js/googlemap.js
@@ -0,0 +1,45 @@
+﻿let map;
+let marker;
+
+function load(id, lat, lng, zoom, markerLat, markerLng) {
+    var mapElement = document.getElementById(id);
+    if (mapElement == null) {
+        return;
+    }
+
+    var mapLatLng = new google.maps.LatLng(lat, lng);
+    var options = {
+        zoom: zoom,
+        center: mapLatLng,
+        mapTypeId: google.maps.MapTypeId.ROADMAP
+    };
+
+    map = new google.maps.Map(mapElement, options);
+
+    var markerLatLng = new google.maps.LatLng(markerLat, markerLng);
+    marker = new google.maps.Marker({
+        position: markerLatLng,
+        map: map
+    });
+
+    google.maps.event.addListener(map, 'click', mapClickEvent);
+}
+
+function mapClickEvent(event) {
+    marker.setMap(null);
+    map.setCenter(event.latLng);
+    marker = new google.maps.Marker({
+        position: event.latLng,
+        map: map
+    });
+}
+
+function getMapInfo() {
+    return {
+        "Zoom": map.getZoom(),
+        "Lat": map.getCenter().lat(),
+        "Lng": map.getCenter().lng(),
+        "MarkerLat": marker.getPosition().lat(),
+        "MarkerLng": marker.getPosition().lng()
+    };
+}
---
+++ wasm_google_map_sample/Shared/GoogleMapInfo.cs
@@ -0,0 +1,15 @@
+﻿namespace wasm_google_map_sample.Shared
+{
+    public class GoogleMapInfo
+    {
+        public int Zoom { get; set; }
+
+        public double Lat { get; set; }
+
+        public double Lng { get; set; }
+
+        public double MarkerLat { get; set; }
+
+        public double MarkerLng { get; set; }
+    }
+}
